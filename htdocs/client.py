ab33ed16bd9a0f91032c334a861e08ffd9ea3449
ambro <spam.amm@outlook.com>
2016-10-30 22:11:52 +0800

    Docu rdy4review & client-launcher
    Draft of documentation is ready for review. Also added a batch & python
    file to only start the search engine (as if it would be a proper
    client/server app)

---
+++ htdocs/client.py
@@ -0,0 +1,14 @@
+# launch browser to use the search engine
+import webbrowser
+#from http.server import HTTPServer, CGIHTTPRequestHandler
+
+#def run(server_class=HTTPServer, handler_class=CGIHTTPRequestHandler):
+#    server_address = ('', 8000)
+#    httpd = server_class(server_address, handler_class)
+#    httpd.serve_forever()
+
+if __name__ == '__main__':
+    url = 'http://localhost:8000'
+    print('Server runs at', url)
+    webbrowser.open(url)
+    run()
